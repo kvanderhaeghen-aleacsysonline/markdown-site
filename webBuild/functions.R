beac4c888369aeae4d8018d3627d53d767c62809
Kristof Van Der Haeghen <k.van.der.haeghen.aleacsys@napoleongames.be>
2024-01-24 16:48:35 +0100

    Adding sections with tables + Trying to add images to the tables.

--- webBuild/functions.R
+++ webBuild/functions.R
@@ -1,12 +1,32 @@
-createCsvTable <- function(path) { 
+createCsvTable <- function(path, display_rows = 15) { 
   library(data.table)
+  library(knitr)
+  library(kableExtra)
+  
   x = readLines(path)
   y = gsub('","', "','", x) # replace double quotes for each field
   y = gsub('^"|"$', "'", y) # replace trailing and leading double quotes
   z = paste(y, collapse='\n') # turn it back into a table for fread to read
+  
   features = fread(z, quote="'")
+  
+  kable(head(features, display_rows)) %>%
+    kable_styling(bootstrap_options = "bordered", latex_options = "striped", full_width = FALSE)
+}
+
+createCsvTableTwo <- function(path, display_rows = 15) { 
+  library(data.table)
   library(knitr)
   library(kableExtra)
-  kable(head(features)) %>%
+  
+  features <- fread(path, quote = "\"")
+  kable(head(features, display_rows), format = "html") %>%
     kable_styling(bootstrap_options = "bordered", latex_options = "striped", full_width = FALSE)
+}
+
+printJson <- function(path) { 
+  library(jsonlite)
+  file_content <- readLines(path)
+  json_content <- toJSON(file_content, pretty = TRUE)
+  cat("```", file_content, "\n```", sep = "\n")
 }
